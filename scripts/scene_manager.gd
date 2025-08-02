30ac7cafd9c9a657a904977e17eaa9a92d7c914f
Brooklyn-Dev <brooklynbaylisdev@gmail.com>
2025-08-02 17:42:11 +0100

    feat: implement basic scene management and tileset

---
+++ scripts/scene_manager.gd
@@ -0,0 +1,27 @@
+extends Node
+
+@export var scenes: Array[PackedScene] = []
+
+var current_scene = null
+var current_index = 0
+
+func _ready():
+	var root = get_tree().root
+	current_scene = root.get_child(root.get_child_count() - 1)
+
+func goto_scene(scene: PackedScene):
+	call_deferred("_goto_scene", scene)
+
+func _goto_scene(scene: PackedScene):
+	current_scene.free()	
+	current_scene = scene.instantiate()
+	get_tree().root.add_child(current_scene)
+
+func goto_next_scene():
+	current_index += 1
+	if current_index >= scenes.size():
+		current_index = 0
+	goto_scene(scenes[current_index])
+
+func restart_current_scene():
+	goto_scene(scenes[current_index])
