2deb4239ac965ac080ad0df27e2969e3c456f7ca
Brooklyn-Dev <brooklynbaylisdev@gmail.com>
2025-08-04 14:10:36 +0100

    chore: remove redundant audio logic

--- scripts/music_manager.gd
+++ scripts/music_manager.gd
@@ -1,7 +1,6 @@
 extends Node
 
 @onready var music_player = AudioStreamPlayer.new()
-var is_muted := false
 
 func _ready():
 	add_child(music_player)
@@ -15,10 +14,6 @@ func _ready():
 func play_music():
 	if not music_player.playing:
 		music_player.play()
-		
+
 func stop_music():
 	music_player.stop()
-
-func toggle_mute():
-	is_muted = !is_muted
-	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), is_muted)
